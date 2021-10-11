a21aa2d67513aec91a16ddc22db7647fe4ba38a2
stefan <stefanace26@gmail.com>
2021-10-11 17:56:53 -0400

    class #10:completando y eliminando ToDos con FindIndex y Splice

--- src/App.js
+++ src/App.js
@@ -41,6 +41,19 @@ function App() {
 		})
 	}
 
+	const completeTodo = (text) =>{
+		const todoIndex = todos.findIndex(todo => todo.text ===text)
+		const newTodos = [...todos]
+		newTodos[todoIndex].completed = true
+		setTodos(newTodos)
+	}
+	const deleteTodo = (text) =>{
+		const todoIndex = todos.findIndex(todo => todo.text ===text)
+		const newTodos = [...todos]
+		newTodos.splice(todoIndex,1)
+		setTodos(newTodos)
+	}
+
   return (
     <React.Fragment>
       <TodoCounter 
@@ -53,7 +66,13 @@ function App() {
 			/>
       <TodoList>
         {searchedTodo.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+          <TodoItem 
+					key={todo.text} 
+					text={todo.text} 
+					completed={todo.completed} 
+					onComplete={()=>completeTodo(todo.text)}
+					onDelete={()=>deleteTodo(todo.text)}
+					/>
         ))}
       </TodoList>
       <CreateTodoButton />
