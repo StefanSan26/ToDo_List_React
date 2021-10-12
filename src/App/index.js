27854d1ba77cfed2831daad51b04f65d073cbfff
stefan <stefanace26@gmail.com>
2021-10-12 11:54:27 -0400

    class #13: Custom Hook para Local Storage

--- src/App/index.js
+++ src/App/index.js
@@ -18,21 +18,35 @@ import { AppUI } from "./AppUI";
 //   },
 // ];
 
-function App() {
+function useLocalStorage(itemName,initialValue){
 
-	const localStorageTodos = localStorage.getItem('TODOS_V1')
-	let parsedTodos
+	const localStorageItem = localStorage.getItem(itemName)
+	let parsedItem
 
-	if (!localStorageTodos) {
-		localStorage.setItem('TODOS_V1','[]')
-		parsedTodos=[]
+	if (!localStorageItem) {
+		localStorage.setItem(itemName,JSON.stringify(initialValue))
+		parsedItem=initialValue
 	} else {
-		parsedTodos = JSON.parse(localStorageTodos)
+		parsedItem = JSON.parse(localStorageItem)
 	}
 
+	const [item, setItem] = React.useState(parsedItem)
 
-	const [todos,setTodos] = React.useState(parsedTodos)
-	const [searchValue, setSearchValue] = React.useState('')
+	const saveItem = (newItem)=>{
+		const stringItem=JSON.stringify(newItem)
+		localStorage.setItem(itemName,stringItem)
+		setItem(newItem)
+	}
+	return[
+		item,
+		saveItem
+	]
+}
+
+function App() {
+	const [todos,saveTodos] = useLocalStorage('TODOS_V1',[])
+
+	const [searchValue,setSearchValue] = React.useState('')
 
 	const completedTodos = todos.filter(todo => !!todo.completed).length
 	const totalTodos = todos.length
@@ -49,11 +63,7 @@ function App() {
 		})
 	}
 
-	const saveTodos = (newTodos)=>{
-		const stringTodos=JSON.stringify(newTodos)
-		localStorage.setItem('TODOS_V1',stringTodos)
-		setTodos(newTodos)
-	}
+	
 
 	const completeTodo = (text) =>{
 		const todoIndex = todos.findIndex(todo => todo.text ===text)
